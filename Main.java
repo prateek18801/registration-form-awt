6093b1e89c6be262b3183f1ea54affc0741cad95
Prateek Chaurasia <prateek18801@gmail.com>
2022-10-11 01:43:05 +0530

    code restructure

--- Main.java
+++ Main.java
@@ -3,17 +3,10 @@ import java.awt.event.*;
 
 class RegistrationForm extends Frame {
 
-	Label title, name, roll, branch, contact, gender, city, username, password, hint, answer;
-	TextField nameField, rollField, contactField, usernameField, passwordField, answerField;
-	Checkbox male, female;
-	CheckboxGroup genderGroup;
-	Choice branchField, cityField, hintField;
-	Button submitBtn;
-
 	RegistrationForm() {
 
 		super("User Registration Form");
-		setSize(1000, 800);
+		setSize(1000, 700);
 		setLayout(null);
 		setVisible(true);
 		Color formColor = new Color(53, 59, 72);
@@ -21,44 +14,44 @@ class RegistrationForm extends Frame {
 
 		Font titleFont = new Font("arial", Font.BOLD, 25);
 		Font labelFont = new Font("arial", Font.PLAIN, 18);
-		Font textFont = new Font("arial", Font.PLAIN, 18);
+		Font textFont = new Font("arial", Font.PLAIN, 15);
 
-		title = new Label("AJAY KUMAR GARG ENGINEERING COLLEGE");
-		title.setBounds(250, 40, 650, 50);
+		Label title = new Label("AJAY KUMAR GARG ENGINEERING COLLEGE");
+		title.setBounds(225, 40, 650, 50);
 		title.setFont(titleFont);
 		title.setForeground(Color.YELLOW);
 		add(title);
 
-		name = new Label("Name :");
-		name.setBounds(250, 100, 150, 30);
+		Label name = new Label("Name :");
+		name.setBounds(200, 100, 150, 30);
 		name.setFont(labelFont);
 		name.setForeground(Color.WHITE);
 		add(name);
 
-		nameField = new TextField();
+		TextField nameField = new TextField();
 		nameField.setBounds(400, 100, 400, 30);
 		nameField.setFont(textFont);
 		add(nameField);
 
-		roll = new Label("Roll No. :");
-		roll.setBounds(250, 150, 150, 30);
+		Label roll = new Label("Roll No. :");
+		roll.setBounds(200, 150, 150, 30);
 		roll.setFont(labelFont);
 		roll.setForeground(Color.WHITE);
 		add(roll);
 
-		rollField = new TextField();
+		TextField rollField = new TextField();
 		rollField.setBounds(400, 150, 400, 30);
 		rollField.setFont(textFont);
 		add(rollField);
 
-		branch = new Label("Branch :");
-		branch.setBounds(250, 200, 150, 30);
+		Label branch = new Label("Branch :");
+		branch.setBounds(200, 200, 150, 30);
 		branch.setFont(labelFont);
 		branch.setForeground(Color.WHITE);
 		add(branch);
 
-		branchField = new Choice();
-		branchField.setFont(labelFont);
+		Choice branchField = new Choice();
+		branchField.setFont(textFont);
 		branchField.setBounds(400, 200, 400, 50);
 		branchField.add("CSE");
 		branchField.add("CS");
@@ -67,45 +60,45 @@ class RegistrationForm extends Frame {
 		branchField.add("Civil");
 		add(branchField);
 
-		contact = new Label("Contact :");
-		contact.setBounds(250, 250, 150, 30);
+		Label contact = new Label("Contact :");
+		contact.setBounds(200, 250, 150, 30);
 		contact.setFont(labelFont);
 		contact.setForeground(Color.WHITE);
 		add(contact);
 
-		contactField = new TextField();
+		TextField contactField = new TextField();
 		contactField.setBounds(400, 250, 400, 30);
 		contactField.setFont(textFont);
 		add(contactField);
 
-		gender = new Label("Gender :");
-		gender.setBounds(250, 300, 150, 30);
+		Label gender = new Label("Gender :");
+		gender.setBounds(200, 300, 150, 30);
 		gender.setFont(labelFont);
 		gender.setForeground(Color.WHITE);
 		add(gender);
 
-		genderGroup = new CheckboxGroup();
+		CheckboxGroup genderGroup = new CheckboxGroup();
 
-		male = new Checkbox("Male", genderGroup, true);
+		Checkbox male = new Checkbox("Male", genderGroup, true);
 		male.setBounds(400, 300, 100, 30);
 		male.setFont(labelFont);
 		male.setForeground(Color.WHITE);
 		add(male);
 
-		female = new Checkbox("Female", genderGroup, false);
+		Checkbox female = new Checkbox("Female", genderGroup, false);
 		female.setBounds(500, 300, 100, 30);
 		female.setFont(labelFont);
 		female.setForeground(Color.WHITE);
 		add(female);
 
-		city = new Label("City :");
-		city.setBounds(250, 350, 150, 30);
+		Label city = new Label("City :");
+		city.setBounds(200, 350, 150, 30);
 		city.setFont(labelFont);
 		city.setForeground(Color.WHITE);
 		add(city);
 
-		cityField = new Choice();
-		cityField.setFont(labelFont);
+		Choice cityField = new Choice();
+		cityField.setFont(textFont);
 		cityField.setBounds(400, 350, 400, 50);
 		cityField.add("Lucknow");
 		cityField.add("Kanpur");
@@ -114,36 +107,36 @@ class RegistrationForm extends Frame {
 		cityField.add("Prayagraj");
 		add(cityField);
 
-		username = new Label("Username :");
-		username.setBounds(250, 400, 150, 30);
+		Label username = new Label("Username :");
+		username.setBounds(200, 400, 150, 30);
 		username.setFont(labelFont);
 		username.setForeground(Color.WHITE);
 		add(username);
 
-		usernameField = new TextField();
+		TextField usernameField = new TextField();
 		usernameField.setBounds(400, 400, 400, 30);
 		usernameField.setFont(textFont);
 		add(usernameField);
 
-		password = new Label("Password :");
-		password.setBounds(250, 450, 150, 30);
+		Label password = new Label("Password :");
+		password.setBounds(200, 450, 150, 30);
 		password.setFont(labelFont);
 		password.setForeground(Color.WHITE);
 		add(password);
 
-		passwordField = new TextField();
+		TextField passwordField = new TextField();
 		passwordField.setBounds(400, 450, 400, 30);
 		passwordField.setFont(textFont);
 		add(passwordField);
 
-		hint = new Label("Hint Question :");
-		hint.setBounds(250, 500, 150, 30);
+		Label hint = new Label("Hint Question :");
+		hint.setBounds(200, 500, 150, 30);
 		hint.setFont(labelFont);
 		hint.setForeground(Color.WHITE);
 		add(hint);
 
-		hintField = new Choice();
-		hintField.setFont(labelFont);
+		Choice hintField = new Choice();
+		hintField.setFont(textFont);
 		hintField.setBounds(400, 500, 400, 50);
 		hintField.add("Name of your first school");
 		hintField.add("Name of your pet");
@@ -151,22 +144,22 @@ class RegistrationForm extends Frame {
 		hintField.add("Name of your favourite sport");
 		add(hintField);
 
-		answer = new Label("Answer :");
-		answer.setBounds(250, 550, 150, 30);
+		Label answer = new Label("Answer :");
+		answer.setBounds(200, 550, 150, 30);
 		answer.setFont(labelFont);
 		answer.setForeground(Color.WHITE);
 		add(answer);
 
-		answerField = new TextField();
+		TextField answerField = new TextField();
 		answerField.setBounds(400, 550, 400, 30);
 		answerField.setFont(textFont);
 		add(answerField);
 
-		submitBtn = new Button("Submit");
-		submitBtn.setBounds(400, 600, 150, 30);
+		Button submitBtn = new Button("Submit");
+		submitBtn.setBounds(400, 600, 130, 35);
 		submitBtn.setFont(labelFont);
-		submitBtn.setBackground(Color.YELLOW);
-		submitBtn.setForeground(Color.WHITE);
+		submitBtn.setBackground(Color.BLACK);
+		submitBtn.setForeground(Color.YELLOW);
 		add(submitBtn);
 
 		this.addWindowListener(new WindowAdapter() {
@@ -175,12 +168,10 @@ class RegistrationForm extends Frame {
 			}
 		});
 	}
-
 }
 
 public class Main {
 	public static void main(String[] args) {
-		RegistrationForm frm = new RegistrationForm();
+		new RegistrationForm();
 	}
-
 }
